651bc565627e2720498a473f266bf6eed130b4ed
Andrew Gross <apgross@udel.edu>
2015-09-20 09:27:14 -0400

    .

--- src/studentTuition/StudentTuition.java
+++ src/studentTuition/StudentTuition.java
@@ -1,5 +1,10 @@
 package studentTuition;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import org.apache.poi.ss.formula.functions.FinanceLib;
+
 public class StudentTuition {
 	private double APR;
 	private int tuition;
@@ -55,15 +60,35 @@ public class StudentTuition {
 	}
 	
 	public double loanAmount() {
-		
+		double pmt = this.tuitionCost() * (((this.getAPR()/100)/12)/(1-(Math.pow(1+(this.getAPR()/100)/12, -(this.getLoanTerm()*12)))));
+		return pmt;
 	}
 	
 	public static void main(String[] args) {
-		StudentTuition test = new StudentTuition();
-		test.setTuition(12520);
-		test.setPerIncr(4.5);
-		System.out.println(test.tuitionCost());
-
+		StudentTuition stuTu = new StudentTuition();
+		Scanner input = new Scanner(System.in);
+		try {
+			System.out.println("Enter tuition cost: ");
+			stuTu.setTuition(input.nextInt());
+			
+			System.out.println("Enter rate tuition rises per year: ");
+			stuTu.setPerIncr(input.nextDouble());
+			
+			System.out.println("Enter APR: ");
+			stuTu.setAPR((input.nextDouble()));
+			
+			System.out.println("Enter # loan repayment years: ");
+			stuTu.setLoanTerm(input.nextInt());
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Wrong data type. Please restart and try again.");
+		}
+		input.close();
+		
+		double totalTuition = stuTu.tuitionCost();
+		double loanPay = stuTu.loanAmount();
+		System.out.printf("Your total tuition will be $%4.2f.", totalTuition);
+		System.out.printf("\nYou will have to pay $%4.2f a month to pay your loans off in %d years.", loanPay, stuTu.getLoanTerm());
 	}
 
 
